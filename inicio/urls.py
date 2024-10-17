f3c7dcd712893fad55d0feab5153a79c924d07f1
Alan <alan.user.1986@gmail.com>
2024-10-16 21:57:12 -0300

    Se arma el esqueleto general de la página con un primer enfoque al proyecto. Se estructura la página para que no genere errores al ingresar a los botones

--- inicio/urls.py
+++ inicio/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
-from inicio.views import inicio, about_me
+from inicio.views import inicio, about_me, Tablero_general, egresos, ingresos, graficas
+from inicio.views import tabla_general
 
 app_name = 'inicio'
 
 urlpatterns = [
     path('', inicio, name='inicio'),
     path('about-me/', about_me, name='about_me'),
+    path('Tablero-general', Tablero_general, name='tablero_general'),
+    path('egresos', egresos, name='egresos'),
+    path('ingresos', ingresos, name='ingresos'),
+    path('graficas', graficas, name='graficas'),
+    path('tabla_general', tabla_general, name='tabla_general'),
 ]
--- inicio/views.py
+++ inicio/views.py
@@ -6,3 +6,18 @@ def inicio(request):
 
 def about_me(request):
     return render(request, 'inicio/about_me.html')
+
+def Tablero_general(request):
+    return render(request, 'inicio/Tablero_general.html')
+
+def egresos(request):
+    return render(request, 'inicio/egresos.html')
+
+def ingresos(request):
+    return render(request, 'inicio/ingresos.html')
+
+def graficas(request):
+    return render(request, 'inicio/graficas.html')
+
+def tabla_general(request):
+    return render(request, 'inicio/tabla_general.html')
